9e7df4b9ceeb1c1b1eb85d9af5d45d9af167ba19
oleksandr <alexkostrytsia@gmail.com>
2024-11-07 01:05:00 +0200

    [feature] : start, also and 2 controllers(Hall, Booking) with health check action Test();

---
+++ BookingHallsSystem.API/Controllers/BookingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingHallsSystem.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]s")]
+public class BookingController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Test()
+    {
+        return Ok("Booking test work..");
+    }
+}
---
+++ BookingHallsSystem.API/Controllers/HallController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingHallsSystem.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]s")]
+public class HallController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Test()
+    {
+        return Ok("Hall test work..");
+    }
+}
